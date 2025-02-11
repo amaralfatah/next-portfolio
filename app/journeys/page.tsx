fcdf3da00f00f779d12cbc85726e6305316a16df
amaralfatah <amaralfatah.main@gmail.com>
2025-02-11 23:55:49 +0700

    fix: deploy

--- app/journeys/page.tsx
+++ app/journeys/page.tsx
@@ -6,7 +6,7 @@ const JourneyPage = () => {
     <div className="flex flex-col gap-16">
       {/* Header */}
       <PageHeader title="My Journey"
-                  description="Here's my journey as a developer, where I learned and grew with each step"/>
+                  description="Here&apos;s my journey as a developer, where I learned and grew with each step"/>
 
       {/* Experience List */}
       <div className="flex flex-col gap-4">
